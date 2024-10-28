205d87015d875ce61e6dca35881752bca5192b97
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-10-28 07:28:14 +0700

    📦 added bottom bar

--- lib/page/home/home_mobile.dart
+++ lib/page/home/home_mobile.dart
@@ -9,10 +9,39 @@ class HomeMobile extends StatefulWidget {
 }
 
 class _HomeMobileState extends State<HomeMobile> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Text("Login"),
+      bottomNavigationBar: BottomAppBar(
+        padding: const EdgeInsets.all(0),
+        elevation: 4,
+        //clipBehavior: Clip.antiAlias,
+        child: SafeArea(
+            child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sell),
+              label: "Produk Baru",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "Wishlist",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
