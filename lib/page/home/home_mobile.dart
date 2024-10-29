eea0ac6aa6267b42d73108e414b01df7da92ec04
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-10-29 10:01:25 +0700

    📦 scaffold app bar

--- lib/page/home/home_mobile.dart
+++ lib/page/home/home_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lagra_client/components/text_field.dart';
 import 'package:provider/provider.dart';
 
 class HomeMobile extends StatefulWidget {
@@ -10,10 +11,33 @@ class HomeMobile extends StatefulWidget {
 
 class _HomeMobileState extends State<HomeMobile> {
   int _currentIndex = 0;
+  TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Login"),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60.0),
+        child: AppBar(
+          title: BorderTextField(
+            controller: _searchController,
+            inputDecoration: const InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(500)),
+              ),
+            ),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+          ],
+          elevation: 4,
+        ),
+      ),
+      body: SafeArea(
+        child: const Text("Yo"),
+      ),
       bottomNavigationBar: BottomAppBar(
         padding: const EdgeInsets.all(0),
         elevation: 4,
@@ -43,5 +67,11 @@ class _HomeMobileState extends State<HomeMobile> {
         )),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 }
